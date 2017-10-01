377cec31e0330a2fe8d499c69a55b00d362b9edc
CodingWorker <dy10010@sina.cn>
2017-10-01 16:08:50 +0800

    add

---
+++ code/JVMDemo/Chapter06/src/main/java/com/test/jvm_study/TestClass.java
@@ -0,0 +1,14 @@
+package com.test.jvm_study;
+
+/**
+ * Created by IntelliJ IDEA
+ * Project: JVMDemo
+ * User: DaiYan
+ * Date: 2017/10/1
+ */
+public final class TestClass {
+    private int m;
+    public int incr(){
+        return m++;
+    }
+}
